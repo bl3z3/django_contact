418d07f1ace0deeadedcc38165678f1f9f40ab23
bl3z3 <nahayo3@gmail.com>
2018-09-04 12:34:21 +0300

    Added logic for sending email and a contact_template for defining the skeleton of the email

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,40 @@
 from contact.forms import ContactForm
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.template import Context
 from django.shortcuts import render
 
 # Create your views here.
 def contact(request):
 	form_class  = ContactForm
 
+	if request.method == 'POST':
+		form = form_class(data=request.POST)
+
+		if form.is_valid():
+			contact_name = form.cleaned_data['contact_name']
+			contact_email = form.cleaned_data['contat_email']
+			form_content = form.cleaned_data['content']
+
+			# email profile with content
+			template = get_template('contact_template.txt')
+
+			context = Context({
+				'contact_name': contact_name,
+				'contact_email': contact_email,
+				'form_content': form_content,
+			})
+			content = template.render(context)
+			email = EmailMessage(
+				'New contact form submission',
+				content,
+				'Your website <hi@weddinglovely.com>',
+				['youremail@gmail.com'],
+				headers = {'Reply-To': contact_email }
+			)
+			email.send()
+			return redirect('contact')
+			
 	return render(request, 'contact.html', {
 		'form': form_class,
 	})
